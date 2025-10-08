21ef0b5e12a897ea8d066030ca0fa45e700c220d
Hayden Carson <riotoushayden@live.com.au>
2025-10-08 14:01:55 +0800

    Update sw.js

--- public/wtm/sw.js
+++ public/wtm/sw.js
@@ -1,6 +1,6 @@
 // Build timestamp - this should be replaced during build process
-const BUILD_TIMESTAMP = '20251008-055844';
-const CACHE_NAME = `walk-to-mordor-20251008-055844`;
+const BUILD_TIMESTAMP = '{{BUILD_TIMESTAMP}}';
+const CACHE_NAME = `walk-to-mordor-{{BUILD_TIMESTAMP}}`;
 const urlsToCache = [
   '/',
   '/wtm/css/main.css',
